87070d9ccfd51fcd7204d7d61cd41c372187c600
Israel Mateos Aparicio <israel4720@gmail.com>
2022-11-25 21:18:47 +0100

    It1 - Agente - V0.1.0
    Implementado Agente

---
+++ Pilgrimfy/it1-servidor/src/main/java/org/it1/servidor/persistencia/Agente.java
@@ -0,0 +1,36 @@
+package org.it1.servidor.persistencia;
+
+public class Agente {
+	private Agente _agente;
+	private Connention _mBD;
+	private String _uRL;
+	private String _driver;
+
+	public Agente getAgente() {
+		return this._agente;
+	}
+
+	public void conectar() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void desconectar() {
+		throw new UnsupportedOperationException();
+	}
+
+	public int insert(String aSQL) {
+		throw new UnsupportedOperationException();
+	}
+
+	public int delete(String aSQL) {
+		throw new UnsupportedOperationException();
+	}
+
+	public int update(String aSQL) {
+		throw new UnsupportedOperationException();
+	}
+
+	public Vector<Object> select(String aSQL) {
+		throw new UnsupportedOperationException();
+	}
+}
