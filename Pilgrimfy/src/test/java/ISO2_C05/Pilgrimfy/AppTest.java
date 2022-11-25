cf5e8efb0bd5c3f3a10f1ca504dfb72da7027c81
Israel Mateos Aparicio <israel4720@gmail.com>
2022-11-25 20:18:30 +0100

    Cambiado arquetipo de proyecto y submódulos
    Cambiado arquetipo de proyecto de java-1.6 a maven-archetype-quickstart
    y creados los submódulos de la iteración divididos en cliente y servidor
    según lo acordado en la reunión #8.

---
+++ Pilgrimfy/src/test/java/ISO2_C05/Pilgrimfy/AppTest.java
@@ -0,0 +1,20 @@
+package ISO2_C05.Pilgrimfy;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
