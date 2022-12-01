fab13f4bdb4059e695a88632359229be7984a98c
AngelLuis1 <AngelLuis.Rodriguez1@alu.uclm.es>
2022-12-01 19:07:05 +0100

    It1 - GestorCaminosTest - V0.1.0

---
+++ Pilgrimfy/it1-servidor/src/test/java/org/it1/servidor/dominio/GestorCaminosTest.java
@@ -0,0 +1,14 @@
+package org.it1.servidor.dominio;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GestorCaminosTest {
+
+	@Test
+	public void testVerCamino() {
+		fail("Not yet implemented");
+	}
+
+}
