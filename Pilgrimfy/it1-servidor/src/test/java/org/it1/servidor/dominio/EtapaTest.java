7f2cc9ea639c21ca7799c6df13e5d982a2404c96
AngelLuis1 <AngelLuis.Rodriguez1@alu.uclm.es>
2022-12-01 19:06:51 +0100

    It1 - EtapaTest - V0.1.0

---
+++ Pilgrimfy/it1-servidor/src/test/java/org/it1/servidor/dominio/EtapaTest.java
@@ -0,0 +1,109 @@
+package org.it1.servidor.dominio;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EtapaTest {
+
+	@Test
+	public void testVerEtapa() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPlanificarEtapa() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetOrigen() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetOrigen() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDestino() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetDestino() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetImagen() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetImagen() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTipo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetTipo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDistancia() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetDistancia() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDificultad() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetDificultad() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDescripcion() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetDescripcion() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetEstado() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetEstado() {
+		fail("Not yet implemented");
+	}
+
+}
