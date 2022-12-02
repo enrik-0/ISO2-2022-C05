ca32f8845d44aeb03e751e70888d26f6ffadf4fc
kik548 <villarceakike@gmail.com>
2022-12-02 16:21:10 +0100

    Negocio 1.1.0

--- Pilgrimfy/it2-servidor/src/main/java/org/it2/servidor/dominio/Negocio.java
+++ Pilgrimfy/it2-servidor/src/main/java/org/it2/servidor/dominio/Negocio.java
@@ -12,22 +12,27 @@ public class Negocio {
 		return this._nombre;
 	}
 
+	
 	public void setNombre(String aNombre) {
 		this._nombre = aNombre;
 	}
 
+	
 	public String getDescripcion() {
 		return this._descripcion;
 	}
 
+	
 	public void setDescripcion(String aDescripcion) {
 		this._descripcion = aDescripcion;
 	}
 
+	
 	public String getHorario() {
 		return this._horario;
 	}
 
+	
 	public void setHorario(String aHorario) {
 		this._horario = aHorario;
 	}
@@ -36,18 +41,22 @@ public class Negocio {
 		return this._localizacion;
 	}
 
+	
 	public void setLocalizacion(String aLocalizacion) {
 		this._localizacion = aLocalizacion;
 	}
 
+	
 	public Boolean getAbierto() {
 		return this._abierto;
 	}
 
+	
 	public void setAbierto(Boolean aAbierto) {
 		this._abierto = aAbierto;
 	}
 
+	
 	public String getContacto() {
 		return this._contacto;
 	}
@@ -56,18 +65,22 @@ public class Negocio {
 		this._contacto = aContacto;
 	}
 
+	
 	public void anadirInformacion(Negocio aNegocio, String aNombre, String aDescripcion, String aHorario, String aLocalizacion, Boolean aAbierto, String aContacto) {
 		throw new UnsupportedOperationException();
 	}
 
+	
 	public void borrarInformacion(Negocio aNegocio, String aNombre, String aDescripcion, String aHorario, String aLocalizacion, Boolean aAbierto, String aContacto) {
 		throw new UnsupportedOperationException();
 	}
 
+	
 	public void editarInformacion(Negocio aNegocio, String aNombre, String aDescripcion, String aHorario, String aLocalizacion, Boolean aAbierto, String aContacto) {
 		throw new UnsupportedOperationException();
 	}
 
+	
 	public Negocio mostrarInformacion(String aNombre) {
 		throw new UnsupportedOperationException();
 	}
