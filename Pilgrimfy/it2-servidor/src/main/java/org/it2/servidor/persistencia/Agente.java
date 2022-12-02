8369cc37cec7ce8a57a83a2a0401fc4a829e3331
kik548 <villarceakike@gmail.com>
2022-12-02 16:20:03 +0100

    Agente 1.1.0

--- Pilgrimfy/it2-servidor/src/main/java/org/it2/servidor/persistencia/Agente.java
+++ Pilgrimfy/it2-servidor/src/main/java/org/it2/servidor/persistencia/Agente.java
@@ -13,27 +13,35 @@ public class Agente {
 		return this._agente;
 	}
 
+	
 	public void conectar() {
 		throw new UnsupportedOperationException();
 	}
 
+	
 	public void desconectar() {
 		throw new UnsupportedOperationException();
 	}
 
+	
 	public int insert(String aSQL) {
 		throw new UnsupportedOperationException();
 	}
 
+	
 	public int delete(String aSQL) {
 		throw new UnsupportedOperationException();
 	}
 
+	
 	public int update(String aSQL) {
 		throw new UnsupportedOperationException();
 	}
 
+	
 	public Vector<Object> select(String aSQL) {
 		throw new UnsupportedOperationException();
 	}
+	
+
 }
