40d57b6547c3ff9c4b7248087b584b95346d2577
kik548 <villarceakike@gmail.com>
2022-12-02 16:18:40 +0100

    GestorNegocio 1.1.0

--- Pilgrimfy/it2-servidor/src/main/java/org/it2/servidor/dominio/GestorNegocio.java
+++ Pilgrimfy/it2-servidor/src/main/java/org/it2/servidor/dominio/GestorNegocio.java
@@ -6,14 +6,17 @@ public class GestorNegocio {
 		throw new UnsupportedOperationException();
 	}
 
+	
 	public void borrarInformacion(Negocio aNegocio, String aNombre, String aDescripcion, String aHorario, String aLocalizacion, Boolean aAbierto, String aContacto) {
 		throw new UnsupportedOperationException();
 	}
 
+	
 	public void editarInformacion(Negocio aNegocio, String aNombre, String aDescripcion, String aHorario, String aLocalizacion, Boolean aAbierto, String aContacto) {
 		throw new UnsupportedOperationException();
 	}
 
+	
 	public Negocio mostrarInformacion(String aNombre) {
 		throw new UnsupportedOperationException();
 	}
