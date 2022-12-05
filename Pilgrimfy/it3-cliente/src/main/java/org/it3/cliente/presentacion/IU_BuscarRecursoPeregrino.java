c454b3176b678b0ff477e4ef96bfc1f1baf6338b
kik548 <villarceakike@gmail.com>
2022-12-05 13:23:47 +0100

    iteracion 3 presentacion

---
+++ Pilgrimfy/it3-cliente/src/main/java/org/it3/cliente/presentacion/IU_BuscarRecursoPeregrino.java
@@ -0,0 +1,16 @@
+package Cliente_Presentacion;
+
+public class IU_BuscarRecursoPeregrino {
+
+	public void mostrarRecursos() {
+		throw new UnsupportedOperationException();
+	}
+
+	public List<Recurso> buscarRecursos(String aNombre) {
+		throw new UnsupportedOperationException();
+	}
+
+	public List<Recurso> buscarRecursosCercanos() {
+		throw new UnsupportedOperationException();
+	}
+}
