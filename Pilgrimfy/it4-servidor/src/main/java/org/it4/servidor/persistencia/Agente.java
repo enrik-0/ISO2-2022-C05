75460be7eda459c2fb6f9dce08978b592937d415
AnaToledano3 <Ana.Toledano@alu.uclm.es>
2022-12-06 13:25:11 +0100

    it4-Agente version 0.1.0

---
+++ Pilgrimfy/it4-servidor/src/main/java/org/it4/servidor/persistencia/Agente.java
@@ -0,0 +1,61 @@
+package Client.ClientPeregrinos.GestionarEtapas.ServidorPersistencia;
+
+public class Agente {
+
+	private Agente agente;
+	private Connention MBD;
+	private String URL;
+	private String Driver;
+
+	public Agente getAgente() {
+		// TODO - implement Agente.getAgente
+		throw new UnsupportedOperationException();
+	}
+
+	public void conectar() {
+		// TODO - implement Agente.conectar
+		throw new UnsupportedOperationException();
+	}
+
+	public void desconectar() {
+		// TODO - implement Agente.desconectar
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param SQL
+	 */
+	public int insert(String SQL) {
+		// TODO - implement Agente.insert
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param SQL
+	 */
+	public int delete(String SQL) {
+		// TODO - implement Agente.delete
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param SQL
+	 */
+	public int update(String SQL) {
+		// TODO - implement Agente.update
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param SQL
+	 */
+	public Vector<Object> select(String SQL) {
+		// TODO - implement Agente.select
+		throw new UnsupportedOperationException();
+	}
+
+}
