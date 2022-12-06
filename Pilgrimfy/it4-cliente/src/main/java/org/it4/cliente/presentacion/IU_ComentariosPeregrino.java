a9ac0542d47ee2adbf6851cf19a8a682d05693e3
AnaToledano3 <Ana.Toledano@alu.uclm.es>
2022-12-06 13:27:12 +0100

    it4-IUComentariosPeregrino version 0.1.0

---
+++ Pilgrimfy/it4-cliente/src/main/java/org/it4/cliente/presentacion/IU_ComentariosPeregrino.java
@@ -0,0 +1,35 @@
+package ClientePresentacion;
+
+import Client.ClientNegocios.Gestionar_comentarios.DominioServidor.*;
+
+public class IU_ComentariosPeregrino {
+
+	/**
+	 * 
+	 * @param comentario
+	 */
+	public void verComentario(Comentario comentario) {
+		// TODO - implement IU_ComentariosPeregrino.verComentario
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param comentario
+	 */
+	public void leerComentario(Comentario comentario) {
+		// TODO - implement IU_ComentariosPeregrino.leerComentario
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param comentario
+	 * @param respuesta
+	 */
+	public void responderComentario(Comentario comentario, string respuesta) {
+		// TODO - implement IU_ComentariosPeregrino.responderComentario
+		throw new UnsupportedOperationException();
+	}
+
+}
