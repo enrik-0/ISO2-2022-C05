0ed86a6c82255e05d1ac69d3c571ea131137bca8
Alzhe1 <aitormillantrujillo@gmail.com>
2022-12-06 23:33:32 +0100

    It3 - InterfazNegocioTest - V0.1.0

---
+++ Pilgrimfy/it2-cliente/src/test/java/org/it2/cliente/presentacion/InterfazNegocioTest.java
@@ -0,0 +1,29 @@
+package org.it2.cliente.presentacion;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class InterfazNegocioTest {
+
+	@Test
+	public void testAnadirInformacion() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testBorrarInformacion() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEditarInformacion() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMostrarInformacion() {
+		fail("Not yet implemented");
+	}
+
+}
