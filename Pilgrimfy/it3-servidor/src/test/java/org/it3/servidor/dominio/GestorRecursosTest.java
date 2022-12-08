1d7118ecddc4646fe631d46d667f15056ed1c46b
Israel Mateos Aparicio <israel4720@gmail.com>
2022-12-08 18:34:09 +0100

    It3 - GestorRecursosTest - V0.1.0

---
+++ Pilgrimfy/it3-servidor/src/test/java/org/it3/servidor/dominio/GestorRecursosTest.java
@@ -0,0 +1,24 @@
+package org.it3.servidor.dominio;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GestorRecursosTest {
+
+	@Test
+	public void testMostrarRecursos() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testBuscarRecursos() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testBuscarRecursosCercanos() {
+		fail("Not yet implemented");
+	}
+
+}
