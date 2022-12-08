eb07a28172393fe1b10e2d34c9621c0bdd2caa1c
Israel Mateos Aparicio <israel4720@gmail.com>
2022-12-08 18:34:50 +0100

    It3 - Google_MapsTest - V0.1.0

---
+++ Pilgrimfy/it3-servidor/src/test/java/org/it3/servidor/dominio/Google_MapsTest.java
@@ -0,0 +1,14 @@
+package org.it3.servidor.dominio;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Google_MapsTest {
+
+	@Test
+	public void testGetCoordenadas() {
+		fail("Not yet implemented");
+	}
+
+}
