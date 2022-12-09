9989a083d96ad07979495c2b117bae6e1bddef90
AnaToledano3 <Ana.Toledano@alu.uclm.es>
2022-12-09 13:44:39 +0100

    it5-GestorLogin v0.1.0

---
+++ Pilgrimfy/it5-servidor/src/main/java/org/it5/servidor/dominio/GestorLogin.java
@@ -0,0 +1,15 @@
+package Client.Autenticar.ServidorDominio;
+
+public class GestorLogin {
+
+	/**
+	 * En el proceso de login se enviará un SMS al usuario para verificar su identidad
+	 * @param usuario
+	 * @param contraseña
+	 */
+	public boolean login(string usuario, string contraseña) {
+		// TODO - implement GestorLogin.login
+		throw new UnsupportedOperationException();
+	}
+
+}
